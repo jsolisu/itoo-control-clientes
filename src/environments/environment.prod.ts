a1b498a733689acb3056681ab32dac0bf0c39d5c
Jean C. Solís <jcsolisu2000@gmail.com>
2020-08-15 23:20:47 -0600

    Configuracion firestore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firestore: {
+    apiKey: 'AIzaSyCGb-ZO-5wxbY-Hj4tzthSyAgzUAFp32Zg',
+    authDomain: 'control-clientes-8f066.firebaseapp.com',
+    databaseURL: 'https://control-clientes-8f066.firebaseio.com',
+    projectId: 'control-clientes-8f066',
+    storageBucket: 'control-clientes-8f066.appspot.com',
+    messagingSenderId: '602414308901',
+    appId: '1:602414308901:web:ea357072f401982100e9cc',
+    measurementId: 'G-MXJRGP4E8D'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firestore: {
+    apiKey: 'AIzaSyCGb-ZO-5wxbY-Hj4tzthSyAgzUAFp32Zg',
+    authDomain: 'control-clientes-8f066.firebaseapp.com',
+    databaseURL: 'https://control-clientes-8f066.firebaseio.com',
+    projectId: 'control-clientes-8f066',
+    storageBucket: 'control-clientes-8f066.appspot.com',
+    messagingSenderId: '602414308901',
+    appId: '1:602414308901:web:ea357072f401982100e9cc',
+    measurementId: 'G-MXJRGP4E8D'
+  }
 };
 
 /*
