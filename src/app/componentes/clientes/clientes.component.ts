24cc8830d2a362083f216138da96c3e7be155f23
Jean C. Solís <jcsolisu2000@gmail.com>
2020-08-16 00:09:11 -0600

    Ajustes a componente clientes

--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ClienteServicio } from '../../servicios/cliente.service';
+import { Cliente } from '../../modelo/cliente.model';
 
 @Component({
   selector: 'app-clientes',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesComponent implements OnInit {
 
-  constructor() { }
+  clientes: Cliente[];
+
+  constructor(private clientesServicio: ClienteServicio) { }
 
   ngOnInit(): void {
+    this.clientesServicio.getClientes().subscribe(
+      clientes => {
+        this.clientes = clientes;
+      }
+    );
+  }
+
+  getSaldoTotal(): number {
+    let saldoTotal = 0;
+    if (this.clientes) {
+      this.clientes.forEach(cliente => {
+        saldoTotal += cliente.saldo;
+      });
+    }
+
+    return saldoTotal;
   }
 
 }
