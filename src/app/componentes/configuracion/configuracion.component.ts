772e7edf92f1e93a77f3a1aab9caad836f1a8918
jsolisu <jcsolisu2000@gmail.com>
2020-08-27 21:22:40 -0600

    Guardar configuracion

--- src/app/componentes/configuracion/configuracion.component.ts
+++ src/app/componentes/configuracion/configuracion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ConfiguracionServicio } from '../../servicios/configuracion.service';
+import { Configuracion } from 'src/app/modelo/configuracion.model';
 
 @Component({
   selector: 'app-configuracion',
@@ -10,11 +12,17 @@ export class ConfiguracionComponent implements OnInit {
 
   permitirRegistro = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private configuracionServicio: ConfiguracionServicio) { }
 
   ngOnInit(): void {
+    this.configuracionServicio.getConfiguracion().subscribe((configuracion: Configuracion) => {
+      this.permitirRegistro = configuracion.permitirRegistro;
+    });
   }
 
   guardar(): void {
+    const configuracion = { permitirRegistro: this.permitirRegistro };
+    this.configuracionServicio.modificarConfiguracion(configuracion);
+    this.router.navigate(['/']);
   }
 }
